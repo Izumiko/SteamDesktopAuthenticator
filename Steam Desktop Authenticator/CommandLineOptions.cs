592cc52d62f5fa14c0349279fdd1853c4a32bf73
Izumiko <yosoro@outlook.com>
2024-06-28 14:50:51 +0800

    Update dependencies

--- Steam Desktop Authenticator/CommandLineOptions.cs
+++ Steam Desktop Authenticator/CommandLineOptions.cs
@@ -12,15 +12,5 @@ namespace Steam_Desktop_Authenticator
         [Option('s', "silent", Required = false,
           HelpText = "Start minimized")]
         public bool Silent { get; set; }
-
-        [ParserState]
-        public IParserState LastParserState { get; set; }
-
-        [HelpOption]
-        public string GetUsage()
-        {
-            return HelpText.AutoBuild(this,
-              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
-        }
     }
 }
